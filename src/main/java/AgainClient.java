0b5157af360c56a53c15586a130d66d1a68c2867
Nimtaa <nima.tavassoli2@gmail.com>
2018-09-26 20:17:00 +0330

    kryo version updated, error not fixed yet

--- src/main/java/AgainClient.java
+++ src/main/java/AgainClient.java
@@ -2,10 +2,10 @@ import org.apache.livy.Job;
 import org.apache.livy.JobContext;
 import org.apache.livy.LivyClient;
 import org.apache.livy.LivyClientBuilder;
-import org.apache.livy.shaded.kryo.kryo.Kryo;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.Serializable;
 import java.net.URI;
@@ -14,20 +14,18 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 public class AgainClient {
-
     private LivyClient client = null;
-    private InnerLivyJob innerjob = null;
-
+    private InnerLivyJob innerjob;
     public AgainClient() {
          innerjob = new InnerLivyJob(1);
         try {
-            client = new LivyClientBuilder().setURI(new URI("http","nima:","127.0.0.1",8998,"","",""))
+            client = new LivyClientBuilder().setURI(new URI("http","nima:ta","127.0.0.1",8998,"","",""))
                     .setConf("livy.rsc.rpc.max.size","2000000000") //enter max limit size
                     .setConf("livy.rsc.channel.log.level","ERROR")
                     .setConf("livy.rsc.server.connect.timeout","150000000ms")
                     .setConf("livy.client.http.connection.timeout","150000000ms")
                     .setConf("livy.spark.master","local")
-                    .setConf("livy.spark.deploy-mode","client")
+//                    .setConf("livy.spark.deploy-mode","client")
                     .build();
         } catch (IOException e) {
             e.printStackTrace();
@@ -40,18 +38,13 @@ public class AgainClient {
         String thisJAR = "./target/apachelivttest-1.0-SNAPSHOT.jar";
         String newJAR = "./out/artifacts/Apache_Livy_Again_jar";
         try {
-            s.client.addJar(new URI(thisJAR));
-        }
-        catch (URISyntaxException e) {
-            e.printStackTrace();
-        }
-        System.err.printf("Running PiJob with %d samples...\n",1);
-        double pi = 0;
-        try {
-            pi = s.client.submit(s.innerjob).get();
+            s.client.uploadJar(new File(thisJAR));
+            System.err.printf("Running PiJob with %d samples...\n",1);
+            double pi = 0;
+            pi = s.client.submit(new LivyJob(1)).get();
             System.out.println("Pi is roughly: " + pi);
         } catch (InterruptedException e) {
-         //   e.printStackTrace();
+            e.printStackTrace();
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
